f8a19c8f107b0cdf2147642089daf0e630fd5193
JasonLG1979 <jasonlevigray3@gmail.com>
2018-01-13 20:08:17 -0600

    Update header and remove "use strict".

--- mprisindicatorbutton@github.io/extension.js
+++ mprisindicatorbutton@github.io/extension.js
@@ -1,23 +1,21 @@
-/* -*- mode: js2; js2-basic-offset: 4; indent-tabs-mode: nil -*- */
-/* jshint esnext: true */
-/* jshint -W097 */
-/* global imports: false */
-/* global global: false */
-/**
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-**/
-"use strict";
+/*
+ * Mpris Indicator Button extension for Gnome Shell 3.26+
+ * Copyright 2018 Jason Gray (JasonLG1979)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
 const Main = imports.ui.main;
 const Gio = imports.gi.Gio;
